12a85cf9e3e73d576d024baf8dbf13bd80a473a9
spezi77 <spezi7713@gmx.net>
2018-08-20 23:11:04 +0200

    Kernel: Update defconfig.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- BoardConfigPixelDust.mk
+++ BoardConfigPixelDust.mk
@@ -4,7 +4,7 @@
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CONFIG := pixeldust_marlin_defconfig
+TARGET_KERNEL_CONFIG := hellspawn_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 5.0.2
