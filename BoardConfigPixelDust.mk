27380c1d42139f10a39dafc69100a8a98a65846f
bdashore3 <bdashore3@gmail.com>
2019-03-08 15:27:38 -0500

    Modify BoardConfig for proper clang compilation
    "Use clang-9.0 from DTC and modify BoardConfig to match"
    
    Please replace **ROM DIR** with the absolute path of the build dir
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- BoardConfigPixelDust.mk
+++ BoardConfigPixelDust.mk
@@ -1,12 +1,15 @@
 # Common board config for marlin, sailfish
 
-# Kernel
-BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CONFIG := king_defconfig
+# Inline kernel
+TARGET_KERNEL_CLANG_VERSION := clang-9.0
+TARGET_KERNEL_CLANG_PATH := **ROM_DIR HERE**/prebuilts/clang/host/$(HOST_OS)-x86/$(TARGET_KERNEL_CLANG_VERSION)/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/google/marlin
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_PATH := ../../../prebuilts/clang/host/linux-x86/clang-9.0/bin
+TARGET_KERNEL_CONFIG := king_defconfig
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
