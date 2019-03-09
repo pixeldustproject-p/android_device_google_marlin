01cfa6f71693c86a2a72c60f9235a314c1c78992
spezi77 <spezi7713@gmx.net>
2019-03-09 17:15:48 +0100

    Let's not use an absolute path for the tc.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- BoardConfigPixelDust.mk
+++ BoardConfigPixelDust.mk
@@ -1,8 +1,9 @@
 # Common board config for marlin, sailfish
+ANDROID_TOP := $(shell pwd)
 
 # Inline kernel
 TARGET_KERNEL_CLANG_VERSION := clang-9.0
-TARGET_KERNEL_CLANG_PATH := /home/kingbri1/pixeldust/prebuilts/clang/host/$(HOST_OS)-x86/$(TARGET_KERNEL_CLANG_VERSION)/bin
+TARGET_KERNEL_CLANG_PATH := $(ANDROID_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(TARGET_KERNEL_CLANG_VERSION)/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CONFIG := king_defconfig
