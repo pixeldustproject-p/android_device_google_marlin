44f11de3603857abaa38be092e01c7165af5dd12
spezi77 <spezi7713@gmx.net>
2019-03-12 22:05:29 +0100

    kKernel target tc: Use aosp clang 9.0.1
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- BoardConfigPixelDust.mk
+++ BoardConfigPixelDust.mk
@@ -1,11 +1,9 @@
 # Common board config for marlin, sailfish
-ANDROID_TOP := $(shell pwd)
 
 # Inline kernel
-TARGET_KERNEL_CLANG_VERSION := clang-9.0
-TARGET_KERNEL_CLANG_PATH := $(ANDROID_TOP)/prebuilts/clang/host/$(HOST_OS)-x86/$(TARGET_KERNEL_CLANG_VERSION)/bin
+TARGET_KERNEL_CLANG_VERSION := 9.0.1
 TARGET_KERNEL_CLANG_COMPILE := true
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_CONFIG := king_defconfig
 TARGET_KERNEL_ARCH := arm64
